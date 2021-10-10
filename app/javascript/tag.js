93dfbe11b540f3e77cac31cf490d459c5b04ea24
yuricca <yuricca531@gmail.com>
2021-10-10 17:14:47 +0900

    非同期通信を利用した入力補助機能の実装

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -8,15 +8,23 @@ if (location.pathname.match("tweets/new")){
       XHR.responseType = "json";
       XHR.send();
       XHR.onload = () => {
-        const tagName = XHR.response.keyword;
         const searchResult = document.getElementById("search-result");
-        tagName.forEach((tag) => {
-          const childElement = document.createElement("div");
-          childElement.setAttribute("class", "child");
-          childElement.setAttribute("id", tag.id);
-          childElement.innerHTML = tag.name;
-          searchResult.appendChild(childElement);
-        });
+        searchResult.innerHTML = "";
+        if (XHR.response){
+          const tagName = XHR.response.keyword;
+          tagName.forEach((tag) => {
+            const childElement = document.createElement("div");
+            childElement.setAttribute("class", "child");
+            childElement.setAttribute("id", tag.id);
+            childElement.innerHTML = tag.name;
+            searchResult.appendChild(childElement);
+            const clickElement = document.getElementById(tag.id);
+            clickElement.addEventListener("click", () => {
+              document.getElementById("tweets_tag_name").value = clickElement.textContent;
+              clickElement.remove();
+            });
+          });
+        };
       };
     });
   });
