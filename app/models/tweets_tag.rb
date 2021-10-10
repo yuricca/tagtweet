04935f120c34db03e762cd513d8570af39a3c965
yuricca <yuricca531@gmail.com>
2021-10-10 14:03:42 +0900

    投稿機能の実装

--- app/models/tweets_tag.rb
+++ app/models/tweets_tag.rb
@@ -10,9 +10,10 @@ class TweetsTag
 
   def save
     tweet = Tweet.create(message: message)
-    tag = Tag.create(name: name)
+    tag = Tag.where(name: name).first_or_initialize
+    tag.save
   
-    TweetTagRelation.create(tweet.id: tweet.id, tag_id: tag.id)
+    TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
   end
-  
+
 end
